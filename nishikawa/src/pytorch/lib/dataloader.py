e5111345124ec1efd1c8e232a270ce9740d862bc
topse31033 <nishikawa.tomoya.000@gmail.com>
2020-01-26 00:41:13 +0900

    パターン検証用にtransformとsampleコードを登録。

--- nishikawa/src/pytorch/lib/dataloader.py
+++ nishikawa/src/pytorch/lib/dataloader.py
@@ -113,6 +113,70 @@ def simple_transform(resize=224):
         transforms.ToTensor()
     ])
 
+### パターン検証用のtransform
+def pattern_transform(resize=224, HorizontalFlip=True, VerticalFlip=True, Rotation=True, Perspective=True, Crop=True, Erasing=True):
+    """
+    DataSet用のtransform
+    
+    Parameters
+    ----------
+    resize : int
+        画像サイズ
+    HorizontalFlip : boolean, default True
+        RandomHorizontalFlipの実行可否
+    VerticalFlip : boolean, default True
+        RandomVerticalFlipの実行可否
+    Rotation : boolean, default True
+        RandomRotationの実行可否
+    Perspective : boolean, default True
+        RandomPerspectiveの実行可否
+    Crop : boolean, default True
+        RandomResizedCropの実行可否
+    Erasing : boolean, default True
+        RandomErasingの実行可否
+
+    Returns
+    -------
+    DataSet用のtransform
+    """
+    transform_list = []
+    
+    # Resize
+    transform_list.append(transforms.Resize((resize, resize)))
+    
+    ### 回転系
+    # HorizontalFlip
+    if HorizontalFlip:
+        transform_list.append(torchvision.transforms.RandomHorizontalFlip(p=0.5))
+    # VerticalFlip
+    if VerticalFlip:
+        transform_list.append(torchvision.transforms.RandomVerticalFlip(p=0.5))
+    # Rotation (degrees=180)
+    if Rotation:
+        transform_list.append(torchvision.transforms.RandomRotation(degrees=180, resample=False, expand=False, center=None))
+
+    ### 遠近
+    # Perspective
+    if Perspective:
+        transform_list.append(torchvision.transforms.RandomPerspective(distortion_scale=0.5, p=0.5, interpolation=3))
+    
+    ### Crop系
+    # ResizedCrop
+    if Crop:
+        transform_list.append(torchvision.transforms.RandomResizedCrop(size=resize, scale=(0.08, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2))
+    
+    # ToTensor
+    transform_list.append(transforms.ToTensor())
+    # Erasing (value=random)
+    if Erasing:
+        transform_list.append(transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value='random', inplace=False))
+    # Normalize (mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+    transform_list.append(transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5), inplace=False))
+    
+    print(transform_list)
+    # transformの定義一覧
+    return transforms.Compose(transform_list)
+
 ### データローダ
 def DataLoader(train_dataset, test_dataset, batch_size=10, suffle=False, num_workers=2):
     """
